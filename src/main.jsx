75c7fc902c2877930cff861ee5a07685b5f4c350
Nick <linshuan880727@gmail.com>
2024-09-20 19:56:21 +0800

    update profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
   import { StrictMode } from 'react'
   import { createRoot } from 'react-dom/client'
   import App from './App.jsx'
-  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+  import { BrowserRouter as Router } from 'react-router-dom';
 
   createRoot(document.getElementById('root')).render(
-    <Router>
-      <App />
-    </Router>,
+    <StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </StrictMode>
   )
